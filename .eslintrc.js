b329a1a2e2a27db4ec6f79c81e174afdd87a3ef1
Jaroslav Kubicek <jaroslav.kubicek@gmail.com>
2020-10-24 22:40:36 +0200

    Add eslint & typescript support

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+};
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  title: "My Site",
+  tagline: "The tagline of my site",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  favicon: "img/favicon.ico",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'Kubajz.dev',
+      title: "Kubajz.dev",
       logo: {
-        alt: 'Kubajz',
-        src: 'img/avatar_small.png',
+        alt: "Kubajz",
+        src: "img/avatar_small.png",
       },
       items: [
         // TODO add here tips from README repo
@@ -22,44 +22,44 @@ module.exports = {
         //   label: 'Docs',
         //   position: 'left',
         // },
-        { to: '/', label: 'Blog', position: 'left' },
+        { to: "/", label: "Blog", position: "left" },
         {
-          href: 'https://github.com/jaroslav-kubicek/blog',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/jaroslav-kubicek/blog",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Medium',
-              href: 'https://medium.com/@jaroslavkubicek',
+              label: "Medium",
+              href: "https://medium.com/@jaroslavkubicek",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/jaroslav-kubicek/',
+              label: "GitHub",
+              href: "https://github.com/jaroslav-kubicek/",
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/kubajzHK',
+              label: "Twitter",
+              href: "https://twitter.com/kubajzHK",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Adeira Universe',
-              href: 'https://github.com/adeira/universe',
+              label: "Adeira Universe",
+              href: "https://github.com/adeira/universe",
             },
             {
-              label: 'Relay Example',
-              href: 'https://relay-example.adeira.now.sh/',
+              label: "Relay Example",
+              href: "https://relay-example.adeira.now.sh/",
             },
           ],
         },
@@ -69,21 +69,19 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/jaroslav-kubicek/blog/edit/master/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/jaroslav-kubicek/blog/edit/master/",
         },
         blog: {
           showReadingTime: true,
-          routeBasePath: '/',
-          editUrl:
-            'https://github.com/jaroslav-kubicek/blog/edit/master/blog/',
+          routeBasePath: "/",
+          editUrl: "https://github.com/jaroslav-kubicek/blog/edit/master/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
