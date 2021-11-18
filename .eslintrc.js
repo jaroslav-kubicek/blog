0dc3c8a0dab0c9baa58e6c2610ba31ad0bbe9b3c
Jaroslav Kubicek <jaroslav.kubicek@gmail.com>
2021-11-19 00:57:34 +0100

    update typescript + eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
 };
